feb37bfdfb15900491a2945f86adb7c029431b12
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-10-31 19:59:41 -0300

    first commit

---
+++ src/main/java/com/example/criptografiaapi/CriptografiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.criptografiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptografiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriptografiaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/criptografiaapi/CriptografiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.criptografiaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptografiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
