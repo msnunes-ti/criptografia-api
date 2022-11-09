9b1d11cd111d5e530506f6c946d5b3ad18345d13
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-11-09 15:25:23 -0300

    Codificar na Versão V2

--- src/main/java/com/example/criptografiaapi/controllers/CifraDeCesarControllerV2.java
+++ src/main/java/com/example/criptografiaapi/controllers/CifraDeCesarControllerV2.java
@@ -1,9 +1,22 @@
 package com.example.criptografiaapi.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.criptografiaapi.dtos.CodificarCifraDeCesarDTO;
+import com.example.criptografiaapi.services.CifraDeCesarServiceV2;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/v2/cifra")
 public class CifraDeCesarControllerV2 {
 
+    @Autowired
+    CifraDeCesarServiceV2 cifraDeCesarService;
+
+    @PostMapping(path = "/codificar")
+    public @ResponseBody void codificarCifraDeCesar(@RequestBody @NotNull CodificarCifraDeCesarDTO codificarCifraDeCesarDTO) {
+        cifraDeCesarService.criptografar(codificarCifraDeCesarDTO);
+    }
 
 }
+
--- src/main/java/com/example/criptografiaapi/services/CifraDeCesarServiceV2.java
+++ src/main/java/com/example/criptografiaapi/services/CifraDeCesarServiceV2.java
@@ -1,9 +1,12 @@
 package com.example.criptografiaapi.services;
 
-import com.example.criptografiaapi.dtos.CifraDeCesarDTO;
 import com.example.criptografiaapi.dtos.CodificarCifraDeCesarDTO;
+import com.example.criptografiaapi.models.CifraDeCesar;
+import com.example.criptografiaapi.repositories.CifraDeCesarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,21 +14,35 @@ import java.util.List;
 @Service
 public class CifraDeCesarServiceV2 {
 
-    public CifraDeCesarDTO criptografar(CodificarCifraDeCesarDTO codificarCifraDeCesarDTO) {
+    @Autowired
+    CifraDeCesarRepository cifraDeCesarRepository;
+
+    public void criptografar(CodificarCifraDeCesarDTO codificarCifraDeCesarDTO) {
         if(codificarCifraDeCesarDTO.getSenha() > 99 || codificarCifraDeCesarDTO.getSenha() < 1) {
             throw new RuntimeException("A senha deve estar entre 0 e 99");
         }
         List<String> letras = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-        int inteiro = (int) codificarCifraDeCesarDTO.getSenha() % letras.size();
-        int codigoValido = ((codificarCifraDeCesarDTO.getSenha() % letras.size()) - inteiro) * letras.size();
-        char[] cifrar = codificarCifraDeCesarDTO.getMensagem().toCharArray();
-        StringBuilder cifrado = new StringBuilder();
-        for (int i = 0; i < cifrar.length; i++) {
-            char novo = cifrar[i];
-
-
+        int parteInteira = codificarCifraDeCesarDTO.getSenha() % letras.size();
+        int codigoValido = ((codificarCifraDeCesarDTO.getSenha() % letras.size()) - parteInteira) * letras.size();
+        StringBuilder mensagemCodificada = new StringBuilder();
+        for (int i = 0; i < codificarCifraDeCesarDTO.getMensagem().length(); i++) {
+            String letraParaCifrar = String.valueOf(codificarCifraDeCesarDTO.getMensagem().charAt(i));
+            int indice = 0;
+            for (int j = 0; j < letras.size(); j++) {
+                if (letras.get(j).equals(letraParaCifrar)) {
+                    indice = j;
+                    break;
+                }
+            }
+            int letraCifrada = indice + codigoValido;
+//            while (letraCifrada > 25) {
+//                letraCifrada -= 26;
+//            }
+            mensagemCodificada.append(letras.get(letraCifrada));
         }
-
-        return null;
+        CifraDeCesar cifraDeCesar = new CifraDeCesar();
+        cifraDeCesar.setMensagem(mensagemCodificada.toString());
+        cifraDeCesar.setSenha(codificarCifraDeCesarDTO.getSenha());
+        cifraDeCesarRepository.save(cifraDeCesar);
     }
 }
