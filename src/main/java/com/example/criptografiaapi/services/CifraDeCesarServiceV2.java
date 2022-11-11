23536f0e9486ca14a53d9fbef221ec1caef9272e
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-11-10 22:08:26 -0300

    Criando Decriptador

--- src/main/java/com/example/criptografiaapi/services/CifraDeCesarServiceV2.java
+++ src/main/java/com/example/criptografiaapi/services/CifraDeCesarServiceV2.java
@@ -1,13 +1,13 @@
 package com.example.criptografiaapi.services;
 
+import com.example.criptografiaapi.dtos.CifraDeCesarDTO;
 import com.example.criptografiaapi.dtos.CodificarCifraDeCesarDTO;
+import com.example.criptografiaapi.dtos.DecodificarCifraDeCesarDTO;
 import com.example.criptografiaapi.models.CifraDeCesar;
 import com.example.criptografiaapi.repositories.CifraDeCesarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,16 +19,14 @@ public class CifraDeCesarServiceV2 {
 
     public void criptografar(CodificarCifraDeCesarDTO codificarCifraDeCesarDTO) {
         if(codificarCifraDeCesarDTO.getSenha() > 99 || codificarCifraDeCesarDTO.getSenha() < 1) {
-            throw new RuntimeException("A senha deve estar entre 0 e 99");
+            throw new RuntimeException("A senha deve estar entre 1 e 99");
         }
         List<String> letras = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
         int parteInteira = codificarCifraDeCesarDTO.getSenha() % letras.size();
-//        int codigoValido = (((codificarCifraDeCesarDTO.getSenha() % letras.size()) * letras.size()) - parteInteira);
         StringBuilder mensagemCodificada = new StringBuilder();
         for (int i = 0; i < codificarCifraDeCesarDTO.getMensagem().length(); i++) {
             int indice = 0;
             String letraParaCifrar = String.valueOf(codificarCifraDeCesarDTO.getMensagem().charAt(i));
-
             for (int j = 1; j < letras.size(); j++) {
                 if (letras.get(j).equalsIgnoreCase(letraParaCifrar)) {
                     indice = j;
@@ -39,9 +37,7 @@ public class CifraDeCesarServiceV2 {
 
             while (letraCifrada > 25) {
                 letraCifrada -= 26;
-
             }
-//            indice = 0;
             if (letraParaCifrar.equals(" ")) {
                 mensagemCodificada.append(" ");
             } else {
@@ -53,4 +49,39 @@ public class CifraDeCesarServiceV2 {
         cifraDeCesar.setSenha(codificarCifraDeCesarDTO.getSenha());
         cifraDeCesarRepository.save(cifraDeCesar);
     }
+
+    public CifraDeCesarDTO decriptografar(DecodificarCifraDeCesarDTO decodificarCifraDeCesarDTO) {
+        if(decodificarCifraDeCesarDTO.getSenha() > 99 || decodificarCifraDeCesarDTO.getSenha() < 1) {
+            throw new RuntimeException("A senha deve estar entre 1 e 99");
+        }
+        List<String> letras = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+        int parteInteira = decodificarCifraDeCesarDTO.getSenha() % letras.size();
+        StringBuilder mensagemDecodificada = new StringBuilder();
+        for (int i = 0; i < decodificarCifraDeCesarDTO.getMensagem().length(); i++) {
+            int indice = 0;
+            String letraParaCifrar = String.valueOf(decodificarCifraDeCesarDTO.getMensagem().charAt(i));
+            for (int j = 1; j < letras.size(); j++) {
+                if (letras.get(j).equalsIgnoreCase(letraParaCifrar)) {
+                    indice = j;
+                    break;
+                }
+            }
+            int letraCifrada = indice - parteInteira;
+
+            while (letraCifrada < 0) {
+                letraCifrada += 26;
+            }
+            if (letraParaCifrar.equals(" ")) {
+                mensagemDecodificada.append(" ");
+            } else {
+                mensagemDecodificada.append(letras.get(letraCifrada));
+            }
+        }
+        CifraDeCesarDTO cifraDeCesarDTO = new CifraDeCesarDTO();
+        cifraDeCesarDTO.setSenha(decodificarCifraDeCesarDTO.getSenha());
+        cifraDeCesarDTO.setMensagem(mensagemDecodificada.toString());
+        return  cifraDeCesarDTO;
+    }
+
+
 }
