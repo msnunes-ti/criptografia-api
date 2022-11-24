4416b740598223405eab25386728f45fd83e43b3
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-11-23 22:05:54 -0300

    Criando UsuarioRepository

--- src/main/java/com/example/criptografiaapi/repositories/UsuarioRepository.java
+++ src/main/java/com/example/criptografiaapi/repositories/UsuarioRepository.java
@@ -4,5 +4,6 @@ import com.example.criptografiaapi.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
+
+    Usuario findByUsuario(String usuario);
 }
