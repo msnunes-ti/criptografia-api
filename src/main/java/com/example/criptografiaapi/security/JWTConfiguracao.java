9c64f9b603b5f9256c71018a70df21117115b5bd
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-12-04 10:39:10 -0300

    Implementando JWT

--- src/main/java/com/example/criptografiaapi/security/JWTConfiguracao.java
+++ src/main/java/com/example/criptografiaapi/security/JWTConfiguracao.java
@@ -32,9 +32,7 @@ public class JWTConfiguracao extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login")
                 .permitAll()
                 .anyRequest().authenticated()
