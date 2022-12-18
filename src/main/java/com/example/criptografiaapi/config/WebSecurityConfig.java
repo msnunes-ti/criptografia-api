b7be945178d9db1fe07d161965ed4949d7e4e155
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-12-18 19:24:11 -0300

    Implementando Web Security

--- src/main/java/com/example/criptografiaapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/criptografiaapi/config/WebSecurityConfig.java
@@ -2,14 +2,14 @@ package com.example.criptografiaapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+//@Configuration
+@Deprecated
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -39,3 +39,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
 }
+
