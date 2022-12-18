fc845b48f44d5c591d9bb54539d3a9b259ab94dd
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-12-18 19:56:14 -0300

    Implementando Web Security

--- src/main/java/com/example/criptografiaapi/config/WebSecurityConfigV2.java
+++ src/main/java/com/example/criptografiaapi/config/WebSecurityConfigV2.java
@@ -2,13 +2,13 @@ package com.example.criptografiaapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+
 public class WebSecurityConfigV2 {
 
     @Bean
@@ -17,16 +17,16 @@ public class WebSecurityConfigV2 {
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers(HttpMethod.GET, "/usuarios/**").hasAnyRole("ROLE_CONVIDADO", "ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.POST, "/usuarios/**").hasAnyRole("ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.PUT, "/usuarios/**").hasAnyRole("ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.PATCH, "/usuarios/**").hasAnyRole("ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.DELETE, "/usuarios/**").hasAnyRole("ROLE_ADMIN")
-                .antMatchers(HttpMethod.GET, "/v2/cifras/**").hasAnyRole("ROLE_CONVIDADO", "ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.POST, "/v2/cifras/**").hasAnyRole("ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.PUT, "/v2/cifras/**").hasAnyRole("ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.PATCH, "/v2/cifras/**").hasAnyRole("ROLE_ADMIN", "ROLE_USER")
-                .antMatchers(HttpMethod.DELETE, "/v2/cifras/**").hasAnyRole("ROLE_ADMIN")
+//                .antMatchers(HttpMethod.GET, "/usuarios/**").hasAnyRole("CONVIDADO", "ADMIN", "USER")
+//                .antMatchers(HttpMethod.POST, "/usuarios/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.PUT, "/usuarios/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.PATCH, "/usuarios/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.DELETE, "/usuarios/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/v2/cifras/**").hasAnyRole("CONVIDADO", "ADMIN", "USER")
+//                .antMatchers(HttpMethod.POST, "/v2/cifras/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.PUT, "/v2/cifras/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.PATCH, "/v2/cifras/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.DELETE, "/v2/cifras/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable();
--- src/main/java/com/example/criptografiaapi/models/RoleModel.java
+++ src/main/java/com/example/criptografiaapi/models/RoleModel.java
@@ -6,7 +6,6 @@ import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;
-import java.io.Serial;
 import java.io.Serializable;
 
 @Entity
@@ -14,8 +13,6 @@ import java.io.Serializable;
 @Setter
 @Table(name = "tb_role")
 public class RoleModel implements GrantedAuthority, Serializable {
-
-    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
