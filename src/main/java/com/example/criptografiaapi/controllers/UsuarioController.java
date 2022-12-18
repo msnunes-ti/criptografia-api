0ef1aeaf15a73ada71694b2f950ed2ca034ee26e
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-12-18 20:15:18 -0300

    Implementando Web Security

--- src/main/java/com/example/criptografiaapi/controllers/UsuarioController.java
+++ src/main/java/com/example/criptografiaapi/controllers/UsuarioController.java
@@ -8,6 +8,7 @@ import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +24,7 @@ public class UsuarioController {
     @Autowired
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @PreAuthorize("hasAnyRole('ROLE_CONVIDADO', 'ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping(path = "/validar-senha")
     public ResponseEntity<TokenDTO> validarSenha(@RequestParam String username, @RequestParam String password){
         try {
@@ -34,16 +36,19 @@ public class UsuarioController {
         }
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_CONVIDADO', 'ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping
     public List<UsuarioSensivelDTO> usuarioSensivelDTOList() {
         return usuarioService.buscarTodosUsuarios();
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_CONVIDADO', 'ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping(path = "/{id}")
     public UsuarioSensivelDTO buscarUsuarioPeloId(@PathVariable Long id) {
         return UsuarioMapper.toUsuarioSensivelDTO(usuarioService.buscarUsuarioPeloId(id));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_CONVIDADO', 'ROLE_USER', 'ROLE_ADMIN')")
     @GetMapping(path = "/existe/{usuario}")
     public ResponseEntity<Object> verificarSeExisteUsuario(@PathVariable String usuario) {
         Boolean usuarioEncontrado = usuarioService.verificarSeExisteUsuario(usuario);
@@ -53,21 +58,25 @@ public class UsuarioController {
         return new ResponseEntity<>(HttpStatus.resolve(204));
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PostMapping
     public void criarUsuario(@RequestBody @NotNull CriarUsuarioDTO criarUsuarioDTO) {
         usuarioService.criarUsuario(criarUsuarioDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PutMapping(path = "/{id}")
     public void atualizarUsuarioCompleto(@PathVariable @NotNull long id, @RequestBody @NotNull AtualizarUsuarioDTO atualizarUsuarioDTO) {
         usuarioService.atualizarUsuarioCompleto(id, atualizarUsuarioDTO);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     @PatchMapping(path = "/{id}")
     public void atualizarSenhaCriptografadaDoUsuario(@PathVariable Long id, @RequestBody @NotNull AtualizarSenhaCriptografadaUsuarioDTO atualizarSenhaCriptografadaUsuarioDTO) {
         usuarioService.atualizarSenhaCriptografadaDoUsuario(id, atualizarSenhaCriptografadaUsuarioDTO.getSenhaCriptografada());
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(path = "/{id}")
     public void deletarUsuario(@PathVariable Long id) {
         usuarioService.deletarUsuario(id);
