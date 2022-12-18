a8e1c30bb5bb15fb2d1857d99b1ecc21ffef16b3
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-12-18 09:19:54 -0300

    Commit

--- src/main/java/com/example/criptografiaapi/config/WebSecurityConfig.java
+++ src/main/java/com/example/criptografiaapi/config/WebSecurityConfig.java
@@ -2,13 +2,16 @@ package com.example.criptografiaapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class SegurancaConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 }
